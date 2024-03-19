96491f2b7d41e4a4b9f523e7f59623e2a0dad0ea
Foued Jridi <fouedjridi@foueds-mbp.home>
2024-03-19 03:53:53 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <HelloWord :sub-title="subTitle" />
+
+</template>
+
+<script setup>
+import HelloWord from "@/components/HelloWord.vue";
+const subTitle = "Lorem ipsum dolor."
+</script>
+
+<style scoped></style>
---
+++ src/components/HelloWord.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Hello World</h1>
+<p>
+{{ props.subTitle }}</p>
+</template>
+
+<script setup>
+const props = defineProps(['subTitle'])
+</script>
+
