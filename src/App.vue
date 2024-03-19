8375d12d3689047d9f1d3e3d7713990dc588cf10
Foued Jridi <fouedjridi@lboufjvrp13.emea.cegedim.grp>
2024-03-19 14:47:30 +0100

    style

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
-  <div>
-    <Header title="Pokemon" />
-  </div>
+
+  <Header title="Pokemon" />
+
+  <Card title="Titre de la carte" :imageUrl="pokemonImg" buttonText="Texte du bouton" @onButtonClick="handleButtonClick"
+    class="img" />
 
 </template>
 
 <script setup>
 import Header from "@/components/Header.vue";
+import Card from "@/components/Card.vue"
+import pokemonImg from "@/assets/pokemon.png"
 const subTitle = "Lorem ipsum dolor."
 </script>
 
-<style scoped></style>
+<style scoped>
+.img {
+  width: 40px;
+}
+</style>
