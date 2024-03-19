5204f2487e1a55d29fd8211b240e7a28de9476da
Foued Jridi <fouedjridi@lboufjvrp13.emea.cegedim.grp>
2024-03-19 18:24:07 +0100

    router

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,14 @@
 
   <Header title="Pokemon" />
 
-  <Card title="Titre de la carte" :imageUrl="pokemonImg" buttonText="Texte du bouton" @onButtonClick="handleButtonClick"
-    class="img" />
-
+  <PokemonList title="Titre de la carte" :imageUrl="pokemonImg" buttonText="Texte du bouton"
+    @onButtonClick="handleButtonClick" class="img" />
+  <router-view></router-view>
 </template>
 
 <script setup>
 import Header from "@/components/Header.vue";
-import Card from "@/components/Card.vue"
+import PokemonList from "@/components/PokemonList.vue"
 import pokemonImg from "@/assets/pokemon.png"
 const subTitle = "Lorem ipsum dolor."
 </script>
