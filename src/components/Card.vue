2f6efaacfd75594e4453cbc75b852373646b5fd0
NonoM33 <renaudlemagicien@gmail.com>
2024-03-19 15:59:55 +0100

    update card pkm

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,73 +1,56 @@
 <template>
-    <div class="card">
-        <div class="single-shop-product">
-            <div class="product-upper">
-                <img :src="imageUrl" alt="product-image" />
-            </div>
-            <h2>
-                <a href="">{{ title }}</a>
-            </h2>
-
-            <div class="product-option-shop">
-                <button class="add_to_cart_button" @click="onButtonClick">
-                    {{ buttonText }}
-                </button>
+    <div class="list">
+        <div v-if="loading">Chargement...</div>
+        <div v-else>
+            <div v-for="(item, index) in itemList" :key="index" class="card">
+                <p>{{ index }} - {{ item.name }}</p>
+                <img :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png?language=fr`" alt="Pokemon">
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-    props: {
-        title: {
-            type: String,
-            required: true
-        },
-        imageUrl: {
-            type: String,
-            required: true
-        },
-        buttonText: {
-            type: String,
-            required: true
-        },
-        onButtonClick: {
-            type: Function,
-            required: true
-        }
+    data() {
+        return {
+            loading: false,
+            itemList: [] // Initialisation de itemList
+        };
+    },
+    mounted() {
+        this.loading = true;
+        axios.get("https://pokeapi.co/api/v2/pokemon")
+            .then(response => {
+                // Supposons que la réponse de l'API est une liste de chaînes de caractères
+                this.itemList = response.data.results; // Remplacer response.data avec la structure correcte des données reçues
+            })
+            .catch(error => {
+                console.error('Une erreur s\'est produite :', error);
+            })
+            .finally(() => {
+                this.loading = false;
+            });
     }
 };
 </script>
 
 <style scoped>
-.single-shop-product h2 {
-    font-size: 20px;
-    margin: 10px 0;
+
+.list {
+    display: flex;
+    margin-left: auto;
+    margin-right: auto;
 }
 
-.product-option-shop {
-    margin-top: 15px;
+.card {
+    background-color: beige;
+    text-align: center;
+    padding: 10px;
+    margin-top: 10px;
+    border-radius: 12px;
 }
 
-.single-shop-product {
-    margin-bottom: 50px
-}
-
-.col2-set {
-    margin-left: 0;
-}
-
-.col-1,
-.col-2 {
-    float: none;
-    margin-left: 0;
-    width: inherit;
-}
-
-.product-upper {
-    width: 250px;
-}
-
-/* Ajoutez d'autres styles CSS selon vos besoins */
 </style>
